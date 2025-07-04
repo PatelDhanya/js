f1253d1b497539f245f228e2cdddf41037f7c1f3
PatelDhanya <pateldhanya10@gmail.com>
2025-07-04 17:44:10 +0000

    conversion of datatype

---
+++ basic/conversionOperators.js
@@ -0,0 +1,29 @@
+let num = 24
+console.log(num)
+console.log(typeof num)
+
+let num1 = "27"
+console.log(num1)
+console.log(typeof num1)
+
+let number = Number(num1)
+console.log(num1)
+console.log(typeof number)
+
+
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+
+ // 1=>true 0=>false
+ // "" => false
+ // "dhanya" => true
+
+ //"27" => 27
+ //"27hbvh" =>NaN
+
+ let someNumber = 23
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
